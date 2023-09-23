63260192d099b7c175cbd4eb79d533b8e11b11d2
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-09-23 23:04:17 +0700

    try fix again

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 // For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-// From: https://github.com/Automattic/node-canvas/issues/1779#issuecomment-895885846
 if (
   process.env.LD_LIBRARY_PATH == null ||
   !process.env.LD_LIBRARY_PATH.includes(
@@ -10,8 +9,6 @@ if (
     process.env.PWD
   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
 }
-
-console.log('LD_LIBRARY_PATH', process.env.LD_LIBRARY_PATH);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
