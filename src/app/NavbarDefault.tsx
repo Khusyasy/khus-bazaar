f3acbf1fdc67d6c8c1d51de5b0d073c135bf8c59
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-09-23 09:19:47 +0700

    create home navbar

---
+++ src/app/NavbarDefault.tsx
@@ -0,0 +1,92 @@
+import { Navbar, Typography, IconButton, Input } from '@/material-tailwind';
+import {
+  ArrowLeftOnRectangleIcon,
+  BookmarkIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+  WrenchScrewdriverIcon,
+} from '@heroicons/react/24/solid';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import Link from 'next/link';
+
+export default async function NavbarDefault() {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <Navbar className="sticky top-4 z-10 mx-auto max-w-screen-xl px-4 py-3 bg-light-green-100 border-green-100">
+      <div className="flex flex-row items-center justify-between gap-y-4 text-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          variant="h6"
+          className="ml-2 cursor-pointer py-1.5"
+        >
+          Khus Perpus
+        </Typography>
+        <form
+          className="relative flex w-max grow mx-8"
+          method="get"
+          action="/search"
+        >
+          <Input
+            type="search"
+            label="Search"
+            name="q"
+            className="pr-20"
+            containerProps={{
+              className: 'min-w-[288px]',
+            }}
+            required
+            crossOrigin="false"
+          />
+          <IconButton
+            size="sm"
+            className="!absolute right-1 top-1 rounded"
+            color="blue-gray"
+            type="submit"
+          >
+            <MagnifyingGlassIcon className="w-5 h-5" />
+          </IconButton>
+        </form>
+        <div className="flex gap-2 md:mr-4">
+          {session?.role === 'ADMIN' && (
+            <Link href="/admin">
+              <IconButton
+                variant="filled"
+                color="blue-gray"
+                title="Admin Dashboard"
+              >
+                <WrenchScrewdriverIcon className="w-5 h-5" />
+              </IconButton>
+            </Link>
+          )}
+          {session ? (
+            <>
+              <Link href="/borrowed">
+                <IconButton
+                  variant="outlined"
+                  color="blue-gray"
+                  title="Borrowed"
+                >
+                  <BookmarkIcon className="w-5 h-5" />
+                </IconButton>
+              </Link>
+              <Link href="/api/auth/signout?callbackUrl=/">
+                <IconButton variant="outlined" color="red" title="Sign Out">
+                  <ArrowLeftOnRectangleIcon className="w-5 h-5" />
+                </IconButton>
+              </Link>
+            </>
+          ) : (
+            <Link href="/api/auth/signin?callbackUrl=/">
+              <IconButton variant="outlined" color="blue-gray" title="Sign In">
+                <UserIcon className="w-5 h-5" />
+              </IconButton>
+            </Link>
+          )}
+        </div>
+      </div>
+    </Navbar>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,61 @@
-import { Button } from "@/material-tailwind";
+import NavbarDefault from './NavbarDefault';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Button</Button>
+    <div className="m-4">
+      <NavbarDefault />
+      <div className="p-10">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+        facilis eaque incidunt. Magnam itaque optio magni accusantium.
+        Repudiandae consequatur iste delectus consectetur quo? Possimus soluta
+        inventore ab saepe, quaerat excepturi ipsam vitae, ipsa optio
+        perferendis officiis suscipit error, eaque corrupti autem magni
+        mollitia! Nisi, debitis hic? Culpa odio assumenda obcaecati sed aut,
+        cupiditate exercitationem aspernatur aliquid tenetur? Consequuntur minus
+        natus velit iusto autem, sint nulla deserunt, eligendi unde itaque
+        obcaecati nisi ea eveniet eos eum quae iste error, tempora ex earum
+        omnis facilis! Distinctio odio temporibus facere quis praesentium
+        eligendi harum natus, a dolorum nihil saepe quod iste ut dolores. C
+      </div>
+      <div className="p-10">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+        facilis eaque incidunt. Magnam itaque optio magni accusantium.
+        Repudiandae consequatur iste delectus consectetur quo? Possimus soluta
+        inventore ab saepe, quaerat excepturi ipsam vitae, ipsa optio
+        perferendis officiis suscipit error, eaque corrupti autem magni
+        mollitia! Nisi, debitis hic? Culpa odio assumenda obcaecati sed aut,
+        cupiditate exercitationem aspernatur aliquid tenetur? Consequuntur minus
+        natus velit iusto autem, sint nulla deserunt, eligendi unde itaque
+        obcaecati nisi ea eveniet eos eum quae iste error, tempora ex earum
+        omnis facilis! Distinctio odio temporibus facere quis praesentium
+        eligendi harum natus, a dolorum nihil saepe quod iste ut dolores.
+      </div>
+      <div className="p-10">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+        facilis eaque incidunt. Magnam itaque optio magni accusantium.
+        Repudiandae consequatur iste delectus consectetur quo? Possimus soluta
+        inventore ab saepe, quaerat excepturi ipsam vitae, ipsa optio
+        perferendis officiis suscipit error, eaque corrupti autem magni
+        mollitia! Nisi, debitis hic? Culpa odio assumenda obcaecati sed aut,
+        cupiditate exercitationem aspernatur aliquid tenetur? Consequuntur minus
+        natus velit iusto autem, sint nulla deserunt, eligendi unde itaque
+        obcaecati nisi ea eveniet eos eum quae iste error, tempora ex earum
+        omnis facilis! Distinctio odio temporibus facere quis praesentium
+        eligendi harum natus, a dolorum nihil saepe quod iste ut dolores.
+      </div>
+      <div className="p-10">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+        facilis eaque incidunt. Magnam itaque optio magni accusantium.
+        Repudiandae consequatur iste delectus consectetur quo? Possimus soluta
+        inventore ab saepe, quaerat excepturi ipsam vitae, ipsa optio
+        perferendis officiis suscipit error, eaque corrupti autem magni
+        mollitia! Nisi, debitis hic? Culpa odio assumenda obcaecati sed aut,
+        cupiditate exercitationem aspernatur aliquid tenetur? Consequuntur minus
+        natus velit iusto autem, sint nulla deserunt, eligendi unde itaque
+        obcaecati nisi ea eveniet eos eum quae iste error, tempora ex earum
+        omnis facilis! Distinctio odio temporibus facere quis praesentium
+        eligendi harum natus, a dolorum nihil saepe quod iste ut dolores.
+      </div>
     </div>
-  )
+  );
 }
