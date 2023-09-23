58b71bc83174a5135b0005a844233a2dc0286c7e
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-09-23 19:54:14 +0700

    borrowed page

---
+++ src/app/borrowed/page.tsx
@@ -0,0 +1,38 @@
+import { Card, Typography } from '@/material-tailwind';
+import NavbarDefault from '@/components/NavbarDefault';
+import prisma from '@/db';
+import BooksContainer from '@/components/BooksContainer';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+
+export default async function Search() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/');
+  }
+
+  const borrowedBooks = await prisma.book.findMany({
+    where: {
+      Borrow: {
+        some: {
+          userId: Number(session.id),
+          status: 'BORROWED',
+        },
+      },
+    },
+  });
+  console.log(JSON.stringify(borrowedBooks));
+  return (
+    <div className="p-4 flex flex-col items-center bg-light-green-300 min-h-screen">
+      <NavbarDefault />
+      <Card className="h-full w-full overflow-scroll mt-4 p-4 max-w-screen-xl bg-light-green-100">
+        <Typography variant="h4" color="blue-gray">
+          Borrowed Books
+        </Typography>
+        <BooksContainer books={borrowedBooks} />
+      </Card>
+    </div>
+  );
+}
